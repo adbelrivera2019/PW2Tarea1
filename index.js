c1eba5fc5edb2030d5a2ca74322aa313e26eaf42
Cristian Rivera <adbel.rivera@unitec.edu>
2023-01-23 22:42:48 -0600

    Funciones
    Creación de las cuatro funciones principales de matematicas

---
+++ index.js
@@ -0,0 +1,24 @@
+  function suma(numero1=23, numero2=10) {
+      return numero1 + numero2;
+
+  }
+
+  function resta(numero1, numero2) {
+      return numero1 - numero2;
+  }
+  function multi(numero1, numero2) {
+    return numero1 * numero2;
+}
+function div(numero1, numero2) {
+    return numero1 / numero2;
+}
+  let n = suma(23, 10);
+  let r = resta(23, 10);
+  let m = multi(23, 10);
+  let d = div(23, 10);
+  console.log("Valor 1: 23");
+  console.log("Valor 2: 10");
+  console.log("La suma es: " + n);
+  console.log("La resta es: " + r);
+  console.log("La multiplicacion es: " + m);
+  console.log("La division es: " + d);
